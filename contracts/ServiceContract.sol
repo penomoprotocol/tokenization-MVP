17a3fd8fce9a2d3d2cdfc00b28881b09560a0ffb
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-08 21:30:02 +0200

    refined service & revenue receiver contracts
    to do
    - check overall logic
    - refine
    - deploy to local env
    - write test scripts

--- contracts/ServiceContract.sol
+++ contracts/ServiceContract.sol
@@ -1,18 +1,27 @@
 pragma solidity ^0.8.0;
 
 import "./TokenContract.sol"; // Import the token contract
+import "./LiquidityContract.sol"; // Import the liquidity contract
+import "./RevenueContract.sol"; // Import the revenue contract
 
 contract ServiceContract {
     address public owner; // Penomo platform's address
     address[] public registeredInvestors; // Whitelist of registered investors
     TokenContract public tokenContract; // Reference to the token contract
+    LiquidityContract public liquidityContract; // Reference to the liquidity contract
+    RevenueContract public revenueContract; // Reference to the revenue contract
+    uint256 public penomoFee; // Penomo fee in basis points (e.g., 500 for 5%)
 
     // Event to log the purchase of tokens
     event TokensPurchased(address indexed investor, uint256 amount);
+    event ReceivedFundsFromRevenueReceiver(address indexed from, uint256 amount);
 
-    constructor(address _tokenContractAddress) {
+    constructor(address _tokenContractAddress, address _liquidityContractAddress, address _revenueContractAddress, uint256 _penomoFee) {
         owner = msg.sender;
         tokenContract = TokenContract(_tokenContractAddress);
+        liquidityContract = LiquidityContract(_liquidityContractAddress);
+        revenueContract = RevenueContract(_revenueContractAddress);
+        penomoFee = _penomoFee;
     }
 
     modifier onlyOwner() {
@@ -34,10 +43,31 @@ contract ServiceContract {
         uint256 requiredEther = amount * tokenContract.tokenPrice();
         require(msg.value == requiredEther, "Incorrect Ether sent");
 
+        // Calculate Penomo's fee and the amount to send to the LiquidityContract
+        uint256 feeAmount = (msg.value * penomoFee) / 10000;
+        uint256 liquidityAmount = msg.value - feeAmount;
+
         // Transfer the tokens to the investor
         tokenContract.transfer(msg.sender, amount);
 
+        // Send the funds to the LiquidityContract
+        payable(address(liquidityContract)).transfer(liquidityAmount);
+
         emit TokensPurchased(msg.sender, amount);
+    }
+
+    function receiveFundsFromRevenueReceiver() external payable {
+        // Ensure only the RevenueReceiverContract can send funds
+        // (You might need to add a check or a modifier for this based on your setup)
+
+        // Calculate Penomo's fee and the amount to send to the RevenueContract
+        uint256 feeAmount = (msg.value * penomoFee) / 10000;
+        uint256 revenueAmount = msg.value - feeAmount;
+
+        // Send the funds to the RevenueContract
+        payable(address(revenueContract)).transfer(revenueAmount);
+
+        emit ReceivedFundsFromRevenueReceiver(msg.sender, msg.value);
     }
 
     function isRegisteredInvestor(address investor) public view returns(bool) {
@@ -49,7 +79,7 @@ contract ServiceContract {
         return false;
     }
 
-    // Allows the owner to withdraw the accumulated Ether
+    // Allows the owner to withdraw the accumulated Ether (Penomo's fees)
     function withdraw() public onlyOwner {
         payable(owner).transfer(address(this).balance);
     }
