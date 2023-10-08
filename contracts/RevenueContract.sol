3f397424cdd984c14a28c98492e1dc320b4dcb25
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-08 20:55:35 +0200

    refined revenue contract
    to do
    - readjust service contract for handling funds and penomo fee
    - refine revenue receiver contract

--- contracts/RevenueContract.sol
+++ contracts/RevenueContract.sol
@@ -1,17 +1,50 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./TokenContract.sol"; // Import the previously defined token contract
+
 contract RevenueContract {
-    address public serviceContractAddress;
+    address public serviceContract; // Address of the ServiceContract
+    TokenContract public tokenContract; // Instance of the token contract
 
-    constructor(address _serviceContractAddress) {
-        serviceContractAddress = _serviceContractAddress;
+    // Events
+    event ReceivedFunds(address indexed from, uint256 amount);
+    event DistributedRevenue(address indexed to, uint256 amount);
+
+    constructor(address _serviceContract, address _tokenContract) {
+        serviceContract = _serviceContract;
+        tokenContract = TokenContract(_tokenContract);
     }
 
-    function distributeRevenueShares() public {
-        // Implementation
+    // Modifier to ensure only the ServiceContract can send funds
+    modifier onlyServiceContract() {
+        require(msg.sender == serviceContract, "Only the ServiceContract can send funds");
+        _;
     }
 
-    function receiveFunds() public payable {
-        // Implementation
+    // Function to receive funds from the ServiceContract
+    function receiveFunds() external payable onlyServiceContract {
+        emit ReceivedFunds(msg.sender, msg.value);
+        distributeRevenue();
+    }
+
+    // Function to distribute revenue among token holders
+    function distributeRevenue() internal {
+        uint256 totalSupply = tokenContract.totalSupply();
+        require(totalSupply > 0, "No tokens in circulation");
+
+        address[] memory tokenHolders = tokenContract.getTokenHolders();
+        for (uint256 i = 0; i < tokenHolders.length; i++) {
+            address holder = tokenHolders[i];
+            uint256 holderBalance = tokenContract.balanceOf(holder);
+            uint256 holderShare = (address(this).balance * holderBalance) / totalSupply;
+            payable(holder).transfer(holderShare);
+            emit DistributedRevenue(holder, holderShare);
+        }
+    }
+
+    // Function to check the balance of the contract
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
