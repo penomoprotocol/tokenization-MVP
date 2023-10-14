001bc2f179eba87402349603c7159a05bc665557
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-14 15:30:38 +0200

    add mongodb, passport, jwt
    to do
    - test and debug

--- api/server.js
+++ api/server.js
@@ -4,6 +4,15 @@ const mongoose = require('mongoose');
 const ipfsClient = require('ipfs-http-client');
 const cors = require('cors');
 
+// Authentication and authorization imports
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+
 const app = express();
 
 // Middleware
@@ -12,108 +21,152 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/bbTokenization', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Initialize IPFS client
 const ipfs = ipfsClient({ host: 'localhost', port: '5001', protocol: 'http' });
 
+
+// JWT configuration
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'YOUR_SECRET_KEY'  // Replace with your secret key
+};
+
+
+// Initialize passport
+passport.use(new JwtStrategy(jwtOptions, (jwtPayload, done) => {
+    // Here, you'd typically look up your user in the database using the info in jwtPayload
+    // For simplicity, we'll just return the jwtPayload as the user
+    return done(null, jwtPayload);
+}));
+
+app.use(passport.initialize());
+
+
+
 // Routes
 
-// Company Routes
-app.post('/company/register', (req, res) => {
-  // Handle company registration logic
+// Protecting a route using JWT authentication
+app.get('/protectedRoute', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.send('This is a protected route!');
 });
 
-app.post('/company/login', (req, res) => {
-  // Handle company login logic
+// Company Routes
+// Register route with password hashing and token generation
+app.post('/company/register', async (req, res) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Store the hashed password in your database along with other user details
+    // ...
+
+    // Return a JWT to the user
+    const token = jwt.sign({ id: 'USER_ID' }, 'YOUR_SECRET_KEY');  // Replace USER_ID with the actual user ID from your database
+    res.json({ token });
 });
+
+// Login route with password verification and token generation
+app.post('/company/login', async (req, res) => {
+    // Fetch the user from your database using req.body.username or email
+    // ...
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, 'STORED_HASHED_PASSWORD');  // Replace STORED_HASHED_PASSWORD with the hashed password from your database
+
+    if (isPasswordValid) {
+        const token = jwt.sign({ id: 'USER_ID' }, 'YOUR_SECRET_KEY');  // Replace USER_ID with the actual user ID from your database
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
+
 
 app.get('/company/:id', (req, res) => {
-  // Retrieve company details
+    // Retrieve company details
 });
 
 app.put('/company/:id', (req, res) => {
-  // Update company details
+    // Update company details
 });
 
 app.delete('/company/:id', (req, res) => {
-  // Delete company
+    // Delete company
 });
 
 // Investor Routes
 app.post('/investor/register', (req, res) => {
-  // Handle investor registration
+    // Handle investor registration
 });
 
 app.post('/investor/login', (req, res) => {
-  // Handle investor login
+    // Handle investor login
 });
 
 app.post('/investor/buyToken', (req, res) => {
-  // Handle token purchase
+    // Handle token purchase
 });
 
 app.get('/investor/:id', (req, res) => {
-  // Retrieve investor details
+    // Retrieve investor details
 });
 
 app.put('/investor/:id', (req, res) => {
-  // Update investor details
+    // Update investor details
 });
 
 app.delete('/investor/:id', (req, res) => {
-  // Delete investor
+    // Delete investor
 });
 
 // Real World Assets Routes
 app.post('/asset/register', (req, res) => {
-  // Register asset and return DID
+    // Register asset and return DID
 });
 
 app.post('/asset/storeData', (req, res) => {
-  // Store asset data and return CID
+    // Store asset data and return CID
 });
 
 app.post('/asset/tokenize', (req, res) => {
-  // Tokenize asset and deploy contracts
+    // Tokenize asset and deploy contracts
 });
 
 app.post('/asset/connectRevenueStream', (req, res) => {
-  // Deploy revenue stream contract and connect to tokenization engine
+    // Deploy revenue stream contract and connect to tokenization engine
 });
 
 app.get('/asset/:did', (req, res) => {
-  // Retrieve asset details
+    // Retrieve asset details
 });
 
 app.put('/asset/:did', (req, res) => {
-  // Update asset details
+    // Update asset details
 });
 
 app.delete('/asset/:id', (req, res) => {
-  // Delete asset
+    // Delete asset
 });
 
 // [Nice to have] Transactions Routes
 app.post('/transactions', (req, res) => {
-  // Log a new transaction
+    // Log a new transaction
 });
 
 app.get('/transactions', (req, res) => {
-  // Retrieve all transactions
+    // Retrieve all transactions
 });
 
 app.get('/transactions/:id', (req, res) => {
-  // Retrieve specific transaction
+    // Retrieve specific transaction
 });
 
 app.get('/transactions/user/:userId', (req, res) => {
-  // Retrieve all transactions for a specific user
+    // Retrieve all transactions for a specific user
 });
 
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
