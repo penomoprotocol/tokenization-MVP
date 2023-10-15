f2a5dcc5541ceccd295c45d755bb28defddb2763
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-15 19:39:45 +0200

    add transaction model

---
+++ database/models/Transaction.js
@@ -0,0 +1,23 @@
+const transactionSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['buy', 'sell', 'revenue-distribution'],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // or 'Investor' or 'Company' depending on setup
+        required: true
+    }
+    // ... 
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
