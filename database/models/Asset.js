882ee287f0e38c6a1481923375a313844b70ffd2
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-15 19:39:16 +0200

    add asset model

---
+++ database/models/Asset.js
@@ -0,0 +1,19 @@
+const assetSchema = new mongoose.Schema({
+    DID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    CID: String,
+    batteryData: {
+        type: Object,
+        required: true
+    },
+    tokenized: {
+        type: Boolean,
+        default: false
+    },
+    // ...
+});
+
+const Asset = mongoose.model('Asset', assetSchema);
