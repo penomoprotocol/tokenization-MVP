d754710869e693485da6cd248fa4b58cc97bf45f
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-10-17 17:51:24 +0200

    debug returning promise
    done -> done()
    
    to do:
    uncomment password encryption & test

--- api/test/test.js
+++ api/test/test.js
@@ -2,6 +2,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server.js'); // Make sure to provide the correct path to your server.js file
 const should = chai.should();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Get variables from .env
+const PORT = process.env.PORT || 3000;
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const MONGO_URI = process.env.MONGO_URI;
 
 chai.use(chaiHttp);
 
@@ -18,8 +25,8 @@ describe('Company Registration API', function () {
                 res.should.have.status(200);
                 res.body.should.be.a('object');
             })
-            .catch(done); // Handle promise rejections by calling done()
+            .catch(done()); // Handle promise rejections by calling done()
     });
-    
+
 });
 
