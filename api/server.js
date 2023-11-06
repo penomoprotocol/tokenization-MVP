b29a5a2830ef2c15025ceefe27d1b162a0186d75
SaschaKubisch <sascha.fh.kubisch@gmail.com>
2023-11-06 17:13:00 +0700

    debug swagger response

--- api/server.js
+++ api/server.js
@@ -1,46 +1,4 @@
-const express = require('express');
-const CryptoJS = require('crypto-js');
-const { web3, networkId, GSCAddress } = require('./config/web3Config');
-const fs = require('fs');
-const path = require('path');
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
-const SECRET_KEY = process.env.SECRET_KEY;
-const MONGO_URI = process.env.MONGO_URI;
-const MASTER_ADDRESS = process.env.MASTER_ADDRESS;
-const MASTER_PRIVATE_KEY = process.env.MASTER_PRIVATE_KEY;
-
-const app = express();
-app.use(express.json());
-
-// Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
-
-
-
-// JWT configuration
-const jwtOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: SECRET_KEY
-};
-
-// Initialize passport
-passport.use(new JwtStrategy(jwtOptions, (jwtPayload, done) => {
-    return done(null, jwtPayload);
-}));
-app.use(passport.initialize());
-
 // Swagger Configuration
 const options = {
     definition: {
@@ -73,23 +31,73 @@ const options = {
 
 const openapiSpecification = swaggerJsdoc(options);
 
+const express = require('express');
+const CryptoJS = require('crypto-js');
+const { web3, networkId, GSCAddress } = require('./config/web3Config');
+const fs = require('fs');
+const path = require('path');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const swaggerUi = require('swagger-ui-express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const SECRET_KEY = process.env.SECRET_KEY;
+const MONGO_URI = process.env.MONGO_URI;
+const MASTER_ADDRESS = process.env.MASTER_ADDRESS;
+const MASTER_PRIVATE_KEY = process.env.MASTER_PRIVATE_KEY;
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+
+
+// JWT configuration
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: SECRET_KEY
+};
+
+// Initialize passport
+passport.use(new JwtStrategy(jwtOptions, (jwtPayload, done) => {
+    return done(null, jwtPayload);
+}));
+app.use(passport.initialize());
+
+
 // Import and use routes
 const companyRoutes = require('./routes/company');
 const investorRoutes = require('./routes/investor');
 const assetRoutes = require('./routes/asset');
 const transactionRoutes = require('./routes/transaction');
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
-
+// API routes
 app.use('/api', companyRoutes);
 app.use('/api', investorRoutes);
 app.use('/api', assetRoutes);
 app.use('/api', transactionRoutes);
 
+// Serve Swagger UI at the root or another route like '/docs'
+app.use('/', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.error('Error starting server:', err);
+    }
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Export modules
-module.exports = { app, companyRoutes, investorRoutes, assetRoutes };
+module.exports = { app, companyRoutes, investorRoutes, assetRoutes, transactionRoutes };
+
