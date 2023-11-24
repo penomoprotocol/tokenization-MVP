aace32b19253cc5e856990f91ec67f608c3e3834
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-11-24 14:59:11 +0700

    refine responsive styling

--- investor-portal/src/components/NavBar.js
+++ investor-portal/src/components/NavBar.js
@@ -10,25 +10,25 @@ const NavBar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                    <Navbar.Brand as={Link} to="/">
-                        <img
-                            src={logo}
-                            className="navbar-logo"
-                            alt="Penomo logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                            <Nav.Link as={Link} to="/marketplace">Marketplace</Nav.Link>
-                            <Nav.Link as={Link} to="/transaction-history">Transaction History</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                            <Nav.Link as={Link} to="/register">Register</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src={logo}
+                        className="navbar-logo"
+                        alt="Penomo logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/marketplace">Marketplace</Nav.Link>
+                        <Nav.Link as={Link} to="/transaction-history">Transaction History</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Link to="/" className="btn-penomo-navbar">Login</Link>
+                        <Link to="/" className="btn-secondary-navbar">Register</Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
