fa3557fa7c7f79854303783edb156689044d6050
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-11-25 11:04:29 +0700

    add AuthProvider to App.js

--- investor-portal/src/App.js
+++ investor-portal/src/App.js
@@ -6,8 +6,6 @@ import Home from './pages/Home';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
 import InvestorDashboard from './pages/InvestorDashboard';
-// import ProfileUpdatePage from './pages/ProfileUpdatePage';
-// import KYCVerificationPage from './pages/KYCVerificationPage';
 import Transactions from './pages/Transactions';
 import Marketplace from './pages/Marketplace';
 
@@ -16,35 +14,37 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
 // Importing auth provider
-import { AuthProvider } from './services/AuthContext';
+import { AuthProvider } from './services/AuthContext'; // Ensure this path is correct
 
 // Importing styling
-import './master.css'
-import './App.css'; 
+import './master.css';
+import './App.css';
 import './components/Footer.css';
 
 function App() {
   return (
-    <div id="root">
-      <div className="footer-container">
-        <div className="content-wrap">
-          <Router>
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<RegistrationPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/dashboard" element={<InvestorDashboard />} />
-              <Route path="/marketplace" element={<Marketplace />} />
-              {/* <Route path="/update-profile" element={<ProfileUpdatePage />} />
-              <Route path="/kyc-verification" element={<KYCVerificationPage />} /> */}
-              <Route path="/transaction-history" element={<Transactions />} />
-            </Routes>
-          </Router>
+    <AuthProvider> {/* Wrap your application with AuthProvider */}
+      <Router>
+        <div id="root">
+          <div className="footer-container">
+            <div className="content-wrap">
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/dashboard" element={<InvestorDashboard />} />
+                <Route path="/marketplace" element={<Marketplace />} />
+                <Route path="/transaction-history" element={<Transactions />} />
+                {/* <Route path="/update-profile" element={<ProfileUpdatePage />} />
+                <Route path="/kyc-verification" element={<KYCVerificationPage />} /> */}
+              </Routes>
+            </div>
+            <Footer />
+          </div>
         </div>
-        <Footer />
-      </div>
-    </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
