64dd43f56dcfba287d85b669a70e62f1d24c036b
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-11-26 16:44:10 +0700

    create mock USDC contract

---
+++ evm-erc20/contracts/USDCContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDCToken is ERC20 {
+    uint256 public constant INITIAL_SUPPLY = 1e9 * (10 ** 18); // 1 billion tokens with 18 decimal places
+
+    constructor(address ownerAddress) ERC20("USDC Token", "USDC") {
+        // Mint the initial supply to the contract creator
+        _mint(msg.sender, INITIAL_SUPPLY);
+
+        // Set the allowance for the ownerAddress
+        _approve(msg.sender, ownerAddress, INITIAL_SUPPLY);
+    }
+}
