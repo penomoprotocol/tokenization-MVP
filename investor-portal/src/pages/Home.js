0269b627daa6edfbf429a2078ac88d7f21233a64
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-12-02 11:31:18 +0700

    refine home page

--- investor-portal/src/pages/Home.js
+++ investor-portal/src/pages/Home.js
@@ -8,18 +8,20 @@ const Home = () => {
       className="home-page-container"
       style={{
         backgroundImage: `url(${homeImg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center top', // This will align the image to the top
-        minHeight: '250vh', // You might want to change this back to 100vh if it's too tall
+        backgroundSize: '90vw auto', // Scales the width to 80% of the viewport width, height is auto to maintain aspect ratio
+        backgroundPosition: 'center', // Keeps the image centered both vertically and horizontally
+        minHeight: '180vh', // Adjust as needed for your design
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
+        backgroundRepeat: 'no-repeat',
       }}
     >
+  
 
       <div className="hero-section" style={{ marginTop: '8rem', padding: '4rem', backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
         <h1 className="section-header">Invest in Energy, Empower the Future</h1>
-        <p>Penomo democratizes investment in energy irastructure, enabling you to directly own a stake and earn revenue from sustainable energy projects. Start investing in a future that pays back — to you and the planet.</p>
+        <p>penomo democratizes investment in energy irastructure, enabling you to directly own a stake and earn revenue from sustainable energy projects. Start investing in a future that pays back — to you and the planet.</p>
       </div>
     </div>
   );
