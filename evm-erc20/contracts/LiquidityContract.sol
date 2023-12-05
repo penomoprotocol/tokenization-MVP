a4ba632273465da7b77c86f61ea3aab2b7e29977
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-12-05 11:55:29 +0700

    modify LC to handle USDC

--- evm-erc20/contracts/LiquidityContract.sol
+++ evm-erc20/contracts/LiquidityContract.sol
@@ -1,60 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract LiquidityContract {
     address public serviceContract; // Address of the ServiceContract
     address public BBWallet; // Address of the Battery Business Wallet
     address public PenomoWallet; // Address of the penomo master wallet
+    address public usdcTokenAddress; // Address of the USDC token contract
 
     // Events
-    event ReceivedFunds(address indexed from, uint256 amount);
-    event WithdrawnFunds(address indexed to, uint256 amount);
+    event ReceivedFunds(address indexed from, uint256 amount, string currency);
+    event WithdrawnFunds(address indexed to, uint256 amount, string currency);
 
     constructor(
         address _serviceContract,
         address _BBWallet,
-        address _PenomoWallet
+        address _PenomoWallet,
+        address _usdcTokenAddress
     ) {
         serviceContract = _serviceContract;
         BBWallet = _BBWallet;
         PenomoWallet = _PenomoWallet;
+        usdcTokenAddress = _usdcTokenAddress;
     }
 
-    // Modifier to ensure only the ServiceContract can send funds
     modifier onlyServiceContract() {
-        require(
-            msg.sender == serviceContract,
-            "Only the ServiceContract can send funds"
-        );
+        require(msg.sender == serviceContract, "Only the ServiceContract can send funds");
         _;
     }
 
-    // Modifier to ensure only the BBWallet or PenomoWallet can withdraw funds
     modifier onlyOwners() {
-        require(
-            msg.sender == BBWallet || msg.sender == PenomoWallet,
-            "Only the BBWallet or PenomoWallet can withdraw funds"
-        );
+        require(msg.sender == BBWallet || msg.sender == PenomoWallet, "Only the BBWallet or PenomoWallet can withdraw funds");
         _;
     }
 
-    // Function to receive funds from the ServiceContract
-    function receiveFunds() external payable {
-        emit ReceivedFunds(msg.sender, msg.value);
+    function receiveFunds() external payable onlyServiceContract {
+        emit ReceivedFunds(msg.sender, msg.value, "ETH");
     }
 
-    // Function to withdraw funds by the BBWallet or PenomoWallet
-    function withdrawFunds(uint256 amount) external onlyOwners {
-        require(
-            address(this).balance >= amount,
-            "Insufficient funds in the contract"
-        );
-        payable(msg.sender).transfer(amount);
-        emit WithdrawnFunds(msg.sender, amount);
+    function receiveUsdcFunds(uint256 amount) external onlyServiceContract {
+        require(IERC20(usdcTokenAddress).transferFrom(msg.sender, address(this), amount), "USDC transfer failed");
+        emit ReceivedFunds(msg.sender, amount, "USDC");
     }
 
-    // Function to check the balance of the contract
-    function getBalance() external view returns (uint256) {
-        return address(this).balance;
+    function withdrawFunds(uint256 amount, bool withdrawInUsdc) external onlyOwners {
+        if (withdrawInUsdc) {
+            require(IERC20(usdcTokenAddress).balanceOf(address(this)) >= amount, "Insufficient USDC funds in the contract");
+            require(IERC20(usdcTokenAddress).transfer(msg.sender, amount), "USDC transfer failed");
+            emit WithdrawnFunds(msg.sender, amount, "USDC");
+        } else {
+            require(address(this).balance >= amount, "Insufficient ETH funds in the contract");
+            payable(msg.sender).transfer(amount);
+            emit WithdrawnFunds(msg.sender, amount, "ETH");
+        }
+    }
+
+    function getBalance() external view returns (uint256 ethBalance, uint256 usdcBalance) {
+        ethBalance = address(this).balance;
+        usdcBalance = IERC20(usdcTokenAddress).balanceOf(address(this));
+    }
+
+    function setUsdcTokenAddress(address _usdcTokenAddress) external onlyOwners {
+        usdcTokenAddress = _usdcTokenAddress;
     }
 }
