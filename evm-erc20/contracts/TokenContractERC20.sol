4ea470d96e554de37677ac59f420c868a6121ea7
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2023-12-05 11:55:05 +0700

    modify TC to handle USDC

--- evm-erc20/contracts/TokenContractERC20.sol
+++ evm-erc20/contracts/TokenContractERC20.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./GlobalStateContract.sol";
 
 contract TokenContractERC20 is ERC20 {
     uint256 public revenueShare; // in basis points (e.g., 500 for 5%)
     uint256 public contractTerm; // in months
     uint256 public maxTokenSupply;
-    uint256 public tokenPrice; // in wei
+    uint256 public tokenPrice; // Price in smallest unit of currency
+    string public acceptedCurrency; // "ETH" or "USDC"
+    address public usdcTokenAddress; // USDC contract address
     GlobalStateContract public globalState;
     address public serviceContract;
     address public penomoWallet;
@@ -16,12 +19,12 @@ contract TokenContractERC20 is ERC20 {
     struct Battery {
         string DID; // Battery DID
         string CID; // Battery Data CID (IPFS)
-        uint256 revenueGoal; // Battery Revenue Goal in wei for contract term
+        uint256 revenueGoal; // Revenue goal in currency's smallest unit for contract term
     }
     Battery[] public batteries;
     address[] public tokenHolders;
 
-    // Events for debugging
+    // Events
     event Debug(uint256 allowance);
 
     struct ConstructorArgs {
@@ -34,6 +37,8 @@ contract TokenContractERC20 is ERC20 {
         uint256 contractTerm;
         uint256 maxTokenSupply;
         uint256 tokenPrice;
+        string currency;
+        address usdcAddress;
     }
 
     constructor(
@@ -49,6 +54,8 @@ contract TokenContractERC20 is ERC20 {
         contractTerm = args.contractTerm;
         maxTokenSupply = args.maxTokenSupply * 10 ** 18;
         tokenPrice = args.tokenPrice;
+        acceptedCurrency = args.currency;
+        usdcTokenAddress = args.usdcAddress;
 
         for (uint i = 0; i < DIDs.length; i++) {
             Battery memory newBattery = Battery({
@@ -59,74 +66,35 @@ contract TokenContractERC20 is ERC20 {
             batteries.push(newBattery);
         }
 
-        // Mint the maximum supply of tokens to the contract's address upon construction
         _mint(address(this), maxTokenSupply);
-
-        // Set the allowance for the ServiceContract
         _approve(address(this), args.serviceContractAddress, maxTokenSupply);
-
-        // Emit allowance for debugging
         emit Debug(allowance(address(this), args.serviceContractAddress));
     }
 
     modifier onlyPenomoWallet() {
-        require(
-            msg.sender == penomoWallet,
-            "Only penomoWallet can execute this"
-        );
+        require(msg.sender == penomoWallet, "Only penomoWallet can execute this");
         _;
     }
 
-    function forceTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) public onlyPenomoWallet {
+    function forceTransfer(address from, address to, uint256 amount) public onlyPenomoWallet {
         _transfer(from, to, amount);
     }
 
-    // Override the transfer function
-    function transfer(
-        address recipient,
-        uint256 amount
-    ) public override returns (bool) {
-        // Call the _beforeTokenTransfer hook
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(recipient);
-
-        // Call the original transfer function from the parent ERC20 contract
         super.transfer(recipient, amount);
-
         return true;
     }
 
-    // Override the transferFrom function
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override returns (bool) {
-        // Call the _beforeTokenTransfer hook
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
         _beforeTokenTransfer(recipient);
-
-        // Call the original transferFrom function from the parent ERC20 contract
         super.transferFrom(sender, recipient, amount);
-
         return true;
     }
 
-    function _beforeTokenTransfer(
-        //address from,
-        address to
-    ) internal //uint256 amount
-    {
-        require(
-            globalState.verifiedInvestors(to),
-            "Recipient is not whitelisted as registered investor."
-        );
-
-        // If the recipient is not already a token holder, add them to the list
+    function _beforeTokenTransfer(address to) internal {
+        require(globalState.verifiedInvestors(to), "Recipient is not whitelisted as registered investor.");
         if (!isTokenHolder(to) && to != address(0)) {
-            // address(0) check is to ensure the zero address is not added
             tokenHolders.push(to);
         }
     }
@@ -140,9 +108,16 @@ contract TokenContractERC20 is ERC20 {
         return false;
     }
 
-    // Function to return the list of token holders
     function getTokenHolders() external view returns (address[] memory) {
         return tokenHolders;
     }
 
+    // Additional functions to handle the currency and USDC address
+    function setCurrency(string memory _currency) external onlyPenomoWallet {
+        acceptedCurrency = _currency;
+    }
+
+    function setUsdcTokenAddress(address _usdcTokenAddress) external onlyPenomoWallet {
+        usdcTokenAddress = _usdcTokenAddress;
+    }
 }
