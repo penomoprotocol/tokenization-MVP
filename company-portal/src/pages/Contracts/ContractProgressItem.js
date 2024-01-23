657d765b35cfb10c1aa23279eee0e6b4fe98fcb8
Sascha Kubisch <sascha.fh.kubisch@gmail.com>
2024-01-23 16:58:13 +0700

    update contract details display
    - remove unnecessary boxes to simplify data visualization

--- company-portal/src/pages/Contracts/ContractProgressItem.js
+++ company-portal/src/pages/Contracts/ContractProgressItem.js
@@ -35,35 +35,35 @@ const ContractProgressItem = ({ contract, onSelect, isSelected }) => {
 
             {isSelected && (
                 <div>
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>Financing Status</h4>
                     <div className={'section-container'}>
-                        <h4>Financing Status</h4>
-                        <div className={'section-container'}>
-                            {contract.statusUpdates && contract.statusUpdates.length > 0 ? (
-                                contract.statusUpdates.map((status, index) => (
-                                    <div className="status-update" key={index}>
-                                        <p><strong>Date:</strong> {formatDate(status.date)}</p>
-                                        <p><strong>Status:</strong> {status.status}</p>
-                                        <p><strong>Messages:</strong> {status.messages.join(', ')}</p>
-                                        {status.actionsNeeded.length > 0 ? (
-                                            <div>
-                                                <p><strong>Actions Needed:</strong> {status.actionsNeeded.join(', ')}</p>
-                                                {status.actionsNeeded.includes('Upload Document') && (
-                                                    <button onClick={() => setShowUploadDocumentModal(true)}>Open</button>
-                                                )}
-                                            </div>
-                                        ) : (
-                                            <p>No actions needed</p>
-                                        )}
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No status updates available</p>
-                            )}
-                        </div>
+                        {contract.statusUpdates && contract.statusUpdates.length > 0 ? (
+                            contract.statusUpdates.map((status, index) => (
+                                <div className="status-update" key={index}>
+                                    <p><strong>Date:</strong> {formatDate(status.date)}</p>
+                                    <p><strong>Status:</strong> {status.status}</p>
+                                    <p><strong>Messages:</strong> {status.messages.join(', ')}</p>
+                                    {status.actionsNeeded.length > 0 ? (
+                                        <div>
+                                            <p><strong>Actions Needed:</strong> {status.actionsNeeded.join(', ')}</p>
+                                            {status.actionsNeeded.includes('Upload Document') && (
+                                                <button onClick={() => setShowUploadDocumentModal(true)}>Open</button>
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <p>No actions needed</p>
+                                    )}
+                                </div>
+                            ))
+                        ) : (
+                            <p>No status updates available</p>
+                        )}
+
                     </div>
 
+
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>Financing Info</h4>
                     <div className='section-container'>
-                        <h4>Financing Info</h4>
                         <p>
                             <strong>Contract Address: </strong>
                             {contract.tokenContractAddress ? (
@@ -81,63 +81,65 @@ const ContractProgressItem = ({ contract, onSelect, isSelected }) => {
                         <p><strong>Share Price: </strong> ${contract.tokenPrice ? contract.tokenPrice : 'N/A'}</p>
                     </div>
 
-                    <div className='section-container'>
-                        <h4>Funds Usage</h4>
-                        {contract.fundingUsage && contract.fundingUsage.length > 0 ? (
-                            contract.fundingUsage.map((usage, index) => (
-                                <div className='section-container' key={index}>
-                                    <span><strong>Type: </strong>{usage.description}</span>
-                                    <span><strong>Amount: </strong>${usage.amount.toLocaleString()}</span>
-                                    <span><strong>Details:</strong> {usage.description}</span>
-                                </div>
-                            ))
-                        ) : (
-                            <p>No fund usage information available</p>
-                        )}
-                    </div>
 
-                    <div className={'section-container'}>
-                        <h4 className={'section-header'}>Projected Revenue</h4>
-                        {contract.revenueStreams && contract.revenueStreams.length > 0 ? (
-                            contract.revenueStreams.map((stream, index) => (
-                                <div className='section-container' key={index}>
-                                    <span><strong>Type: </strong>{stream.name}</span>
-                                    <span><strong>Amount:</strong> ${stream.amount.toLocaleString()}</span>
-                                    <span><strong>Details:</strong> {stream.details}</span>
-                                </div>
-                            ))
-                        ) : (
-                            <p>No revenue streams available</p>
-                        )}
-                    </div>
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>Funds Usage</h4>
 
-                    <div className={'section-container'}>
-                        <h4>Associated Assets</h4>
-                        {contract.associatedAssets && contract.associatedAssets.length > 0 ? (
-                            contract.associatedAssets.map((asset, index) => (
-                                <div className={'section-container'} key={index}>
-                                    <strong className={'section-header'}>
-                                        {`Serial Number: ${asset.serialNumber} (DID: ${shortenDID(asset.DID.value)})`}
-                                    </strong>
-                                    <p>Asset Type: {asset.assetType}</p>
-                                    <p>Brand: {asset.brand}</p>
-                                    <p>Model: {asset.model}</p>
-                                    <p>Capacity (kWh): {asset.capacity}</p>
-                                    <p>Power (kW): {asset.power}</p>
-                                    {/* Add more asset details as needed */}
-                                </div>
-                            ))
-                        ) : (
-                            <p>No associated assets available</p>
-                        )}
-                    </div>
+                    {contract.fundingUsage && contract.fundingUsage.length > 0 ? (
+                        contract.fundingUsage.map((usage, index) => (
+                            <div className='section-container' key={index}>
+                                <span><strong>Type: </strong>{usage.description}</span>
+                                <span><strong>Amount: </strong>${usage.amount.toLocaleString()}</span>
+                                <span><strong>Details:</strong> {usage.description}</span>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No fund usage information available</p>
+                    )}
 
+
+                    <h4  style={{marginTop:'2rem', marginBottom:'1rem'}} className={'section-header'}>Projected Revenue</h4>
+
+                    {contract.revenueStreams && contract.revenueStreams.length > 0 ? (
+                        contract.revenueStreams.map((stream, index) => (
+                            <div className='section-container' key={index}>
+                                <span><strong>Type: </strong>{stream.name}</span>
+                                <span><strong>Amount:</strong> ${stream.amount.toLocaleString()}</span>
+                                <span><strong>Details:</strong> {stream.details}</span>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No revenue streams available</p>
+                    )}
+
+
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>Associated Assets</h4>
+
+                    {contract.associatedAssets && contract.associatedAssets.length > 0 ? (
+                        contract.associatedAssets.map((asset, index) => (
+                            <div className={'section-container'} key={index}>
+                                <strong className={'section-header'}>
+                                    {`Serial Number: ${asset.serialNumber} (DID: ${shortenDID(asset.DID.value)})`}
+                                </strong>
+                                <p>Asset Type: {asset.assetType}</p>
+                                <p>Brand: {asset.brand}</p>
+                                <p>Model: {asset.model}</p>
+                                <p>Capacity (kWh): {asset.capacity}</p>
+                                <p>Power (kW): {asset.power}</p>
+                                {/* Add more asset details as needed */}
+                            </div>
+                        ))
+                    ) : (
+                        <p>No associated assets available</p>
+                    )}
+
+
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>General Info</h4>
                     <div className={'section-container'}>
-                        <h3>General Info</h3>
                         <p>{contract.projectDescription}</p>
                     </div>
+
+                    <h4 style={{marginTop:'2rem', marginBottom:'1rem'}}>Documents</h4>
                     <div className={'section-container'}>
-                        <h3>Documents</h3>
                         <label href="/path/to/prospectus_template.pdf" download className="btn-link">Prospectus</label>
                         <label href="/path/to/prospectus_template.pdf" download className="btn-link">penomo Terms & Conditions</label>
                         <label href="/path/to/prospectus_template.pdf" download className="btn-link">Cost Informations</label>
