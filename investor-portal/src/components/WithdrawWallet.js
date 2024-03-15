ff14c0828427efdeaa191dd39f28ea4b505e1a82
Neeraj GS <gsneeraj05@gmail.com>
2024-03-15 10:20:50 +0530

    Fixed Route to Withdraw or Transfer tokens from Investor

--- investor-portal/src/components/WithdrawWallet.js
+++ investor-portal/src/components/WithdrawWallet.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Modal } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
 
-const WithdrawWallet = ({ currency, closeModal, show }) => {
+const WithdrawWallet = ({ currency, closeModal, show,handleClose }) => {
   const [amount, setAmount] = useState('');
   const [walletAddress, setWalletAddress] = useState('');
   const [status, setStatus] = useState('idle');
   const [error, setError] = useState('');
   const userToken = localStorage.getItem('authToken');
 
+  // handleClose();
+
   const handleTransfer = async () => {
     if (status === 'pending') return;
 
     try {
       setStatus('pending');
-      await axios.post(`${process.env.REACT_APP_PENOMO_API}/api/company/transfer`, {
+      await axios.post(`${process.env.REACT_APP_PENOMO_API}/api/investor/transfer`, {
         amount,
         currency,
         walletAddress
@@ -51,6 +54,10 @@ const WithdrawWallet = ({ currency, closeModal, show }) => {
         {error && <div className="alert alert-danger" role="alert">{error}</div>}
       </Modal.Body>
       <Modal.Footer>
+        {/* //TODO: Need to add a Close Button After Transfer of Token is Succesful */}
+      {/* <Link className="btn-secondary-navbar" onClick={handleClose}>
+          Close
+        </Link> */}
         <button className={`btn-penomo ${status === 'pending' ? 'btn-penomo-secondary' : ''}`}
                 onClick={handleTransfer}>
           {status === 'pending' ? 'Mining blocks...' : status === 'complete' ? 'Transfer complete.' : 'Transfer'}
